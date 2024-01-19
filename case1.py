0759e99659a7169978c234f7443a270b507d043f
eg1to <pedro.h.egito@gmail.com>
2024-01-19 13:38:41 -0300

    case 1 pronto

--- case1.py
+++ case1.py
@@ -1,5 +1,5 @@
 jogos_favoritos = []
 # Como adiciono 'The Witcher', 'FIFA' e 'Minecraft' aqui?
 
-jogos_favoritos = ["The Witcher", "FIFA", "Mincraft"]
+jogos_favoritos = ["The Witcher", "FIFA", "Minecraft"]
 print(jogos_favoritos)
