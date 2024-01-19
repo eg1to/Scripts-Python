bfa379bfcf3f229e2aa80d7b92dee97856bcc400
eg1to <pedro.h.egito@gmail.com>
2024-01-19 13:35:03 -0300

    case 2 pronto

--- case2.py
+++ case2.py
@@ -1,3 +1,8 @@
 notas = [7.5, 8.0, 6.5, 9.0, 8.5]
 # Como faço para calcular a média dessas notas?
+media = sum(notas) / len(notas)
+print(media)
 # Além disso, gostaria de saber como adicionar uma nova nota na lista e recalcular a média.
+notas.append(10)
+media = sum(notas) / len(notas)
+print(media)
